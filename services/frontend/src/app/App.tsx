03718b0bbe460b340974d08b5c6fd21bf205a29e
Hoang Thinh <jonhnathankevin@yahoo.com.vn>
2022-11-25 10:24:23 +0700

    [FEAT] update front end

--- services/frontend/src/app/App.tsx
+++ services/frontend/src/app/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import './landing/landing'
-import Landing from './landing/landing';
+import AuthFrame from './auth-frame/AuthFrame';
+import AuthForm from './auth/AuthForm';
+import ErrorPage from './error/Error';
+import ForgotPassword from './forgot-password/ForgotPassword';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/auth',
+      element: <AuthFrame child={<AuthForm />} />,
+    },
+    {
+      path: '/forgot-password',
+      element: <AuthFrame child={<ForgotPassword />} />,
+    },
+    {
+      path: "*",
+      element: <ErrorPage />,
+    }], {
+
+  });
+
   return (
-    <div className="App">
-      <Landing/>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
